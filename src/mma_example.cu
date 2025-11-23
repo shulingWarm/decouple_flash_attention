880bd3b3d71d27951c04452fbf9416f27647b9bd
zSeal <greatMailForZzh@163.com>
2025-11-23 20:23:32 +0800

    验证ptx示例指令通过

--- src/mma_example.cu
+++ src/mma_example.cu
@@ -1,6 +1,7 @@
 #include"rand_generator.h"
 #include"cuda_check.h"
 #include <cutlass/bfloat16.h>
+#include<cmath>
 
 using bf16 = cutlass::bfloat16_t;
 using u32 = unsigned int;
@@ -10,12 +11,17 @@ static constexpr int N = 8;
 static constexpr int K = 16;
 
 // 用于随机初始化CPU矩阵的函数
-void init_matrix(float *matrix, int rows, int cols, UniformRandomGenerator& generator) {
+template<class T>
+void init_matrix(T *matrix, int rows, int cols, UniformRandomGenerator& generator) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            matrix[i * cols + j] = generator.generate(0, 10);
+            matrix[i * cols + j] = (T)generator.generate(-0.5, 0.5);
+            std::cout<<(float)matrix[i * cols + j]<<" ";
         }
+        std::cout<<std::endl;
     }
+
+    std::cout<<"-------------------------"<<std::endl;
 }
 
 // 测试ptx指令的核函数
@@ -27,14 +33,16 @@ __global__ void test_ptx_kernel(u32 *A, bf16 *B, float *C, float *dst) {
     u32 b_fragment[2];
     // 准备c矩阵和d矩阵的切片
     float c_fragment[4];
-    float d_fragment[4];
+    float d_fragment[4] = {0,0,0,0};
+    // abcd矩阵的排布参考: 
+    // https://docs.nvidia.com/cuda/parallel-thread-execution/#warp-level-matrix-fragment-mma-16816-float
     // 当前线程在块内的偏移量
     int in_block_row = threadIdx.x/4;
     int in_block_col = threadIdx.x%4;
     #pragma unroll
     for (int i = 0; i < 4; i++) {
-        int block_row = i/2;
-        int block_col = i%2;
+        int block_row = i%2;
+        int block_col = i/2;
 
         int element_row = block_row*8 + in_block_row;
         int element_col = block_col*4 + in_block_col;
@@ -87,13 +95,37 @@ __global__ void test_ptx_kernel(u32 *A, bf16 *B, float *C, float *dst) {
     }
 }
 
+// CPU的矩阵相乘函数，用于验证计算结果
+void cpu_gemm(bf16 *A, bf16 *B, float *C, float* D, int M, int N, int K) {
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            float sum = 0;
+            for (int k = 0; k < K; k++) {
+                sum += A[i * K + k] * B[k * N + j];
+            }
+            D[i * N + j] = sum + C[i * N + j];
+        }
+    }
+}
+
+// 计算两个矩阵的误差
+void print_diff(float *cpu_result, float *gpu_result, int M, int N) {
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            float diff = std::abs(cpu_result[i * N + j] - static_cast<float>(gpu_result[i * N + j]));
+            printf("%.5g ", diff);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     // 构造随机数生成器
     UniformRandomGenerator generator;
 
     // 创建CPU矩阵 A, B, C
-    float *A = new float[M * K];
-    float *B = new float[K * N];
+    bf16 *A = new bf16[M * K];
+    bf16 *B = new bf16[K * N];
     float *C = new float[M * N];
 
     // 用于存储结果的dst矩阵
@@ -103,6 +135,9 @@ int main() {
     init_matrix(A, M, K, generator);
     init_matrix(B, K, N, generator);
     init_matrix(C, M, N, generator);
+
+    // 计算cpu的参考答案 
+    cpu_gemm(A, B, C, cpu_dst, M, N, K);
 
     // 把A,B,C从CPU拷贝到GPU malloc的时候加上错误检查
     // 创建bf16的数据类型
@@ -119,4 +154,16 @@ int main() {
     // 初始化gpu的存储结果的指针
     float *d_dst;
     cuda_check(cudaMalloc(&d_dst, M * N * sizeof(float)));
+    // 调用核函数
+    test_ptx_kernel<<<1, 32>>>((u32*)d_A, d_B, d_C, d_dst);
+    // 把gpu的计算结果转到cpu上
+    float cpu_result[M * N];
+    cuda_check(cudaMemcpy(cpu_result, d_dst, M * N * sizeof(float), cudaMemcpyDeviceToHost));
+    cuda_check(cudaFree(d_A));
+    cuda_check(cudaFree(d_B));
+    cuda_check(cudaFree(d_C));
+    cuda_check(cudaFree(d_dst));
+
+    // 打印两个矩阵的误差
+    print_diff(cpu_dst, cpu_result, M, N);
 }
